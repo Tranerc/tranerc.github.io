84930cbf2fcc6607502a4d2032854880685a8758
Tranerc <83982009+Tranerc@users.noreply.github.com>
2021-05-15 10:57:06 +0300

    Add files via upload

---
+++ success.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+<body>
+	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '<?php $fbpix = $_GET["fbpix"]; echo "$fbpix"; ?>');
+	  fbq('track', 'Purchase', {currency: "USD", value: 80.00});
+	</script>
+	<noscript>
+	  <img height="1" width="1" style="display:none" 
+	       src="https://www.facebook.com/tr?id=<?php $fbpix = $_GET["fbpix"]; echo "$fbpix"; ?>&ev=Lead&noscript=1"/>
+	</noscript>
+	<!-- End Facebook Pixel Code -->
+</body>
+</html>
